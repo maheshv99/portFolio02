c83c73eaf9bf84ce3ee621d1b5b1e774969eed29
maheshv99 <mahesh10082001@gmail.com>
2023-04-03 17:16:38 +0530

    modification

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,6 +6,8 @@ import back from "../../Assets/Projects/back.png";
 import clock from "../../Assets/Projects/digital_Clock.jpg";
 import todo from "../../Assets/Projects/TODO-APP.jpg";
 import rblog from "../../Assets/Projects/blog-app.jpg";
+import ecommerce_app from "../../Assets/Projects/ecommerce_app.jpg";
+import trello from "../../Assets/Projects/trello.jpg";
 
 function Projects() {
   return (
@@ -19,6 +21,29 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+
+        <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={ecommerce_app}
+              isBlog={false}
+              title="Blog App"
+              description="While developing this project you will understand that how you can create multiple components in React like Smart Components and Dummy Components and how to modify and do data operations within component using state variables and how to communicate between two components props variables"
+              link="https://github.com/maheshv99/Ecommerce_FrontEnd"
+              link1="https://lucent-cupcake-0e8f88.netlify.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={trello}
+              isBlog={false}
+              title="Trello App"
+              description="Trllo is managing the all project in one side and keep it simple it there in when were you want you can access"
+              link="https://github.com/maheshv99/trello_FrontEnd"
+              link1="https://zippy-pie-270bdf.netlify.app/"
+            />
+          </Col>
+
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={ rblog}
